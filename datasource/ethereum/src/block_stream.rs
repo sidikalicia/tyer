0b7db7feef3a6ede58929c9d553210a4d1c10fdb
Jorge Olivero <jorgeaolivero@gmail.com>
2019-04-03 18:00:43 -0500

    datasource/ethereum: Tune chunk sizes for calls and logs

--- datasource/ethereum/src/block_stream.rs
+++ datasource/ethereum/src/block_stream.rs
@@ -347,7 +347,7 @@ where
 
                         // But also avoid having too large a range to ensure subgraph block ptr is
                         // updated frequently.
-                        let to = cmp::min(from + (100_000 - 1), to_limit);
+                        let to = cmp::min(from + (10_000 - 1), to_limit);
 
                         debug!(ctx.logger, "Finding next blocks with relevant events...");
                         Box::new(
@@ -539,7 +539,7 @@ where
             ReconciliationStep::AdvanceToDescendantBlock { from, to } => {
                 debug!(
                     ctx.logger,
-                    "Skipping {} block(s) with no relevant events...",
+                    "Skipping {} block(s) with no relevant triggers...",
                     to.number - from.number
                 );
 
@@ -579,7 +579,7 @@ where
                                      // that we can skip.
                                      debug!(
                                          ctx.logger,
-                                         "Skipping {} block(s) with no relevant events...",
+                                         "Skipping {} block(s) with no relevant triggers...",
                                          descendant_parent_ptr.number - subgraph_ptr.number
                                      );
 
