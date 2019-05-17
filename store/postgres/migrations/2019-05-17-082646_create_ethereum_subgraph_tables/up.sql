6942594bc53eaa2bc4b43908776ec4850ddc74ee
Jannis Pohlmann <jannis@thegraph.com>
2019-05-17 11:39:02 +0200

    WIP: store/postgres: Add migration to create ethereum tables

---
+++ store/postgres/migrations/2019-05-17-082646_create_ethereum_subgraph_tables/up.sql
@@ -0,0 +1,82 @@
+create table ethereum_mainnet.blocks (
+  id                bytea primary key not null,
+
+  hash              bytea not null,
+  number            numeric,
+  timestamp         numeric not null,
+  author            bytea not null references ethereum_mainnet.accounts(id),
+  nonce             bytea not null,
+
+  parent            bytea not null references ethereum_mainnet.blocks(id),
+  uncles_hash       bytea not null,
+  -- TODO: Account for `uncles`
+
+  state_root        bytea not null,
+  transactions_root bytea not null,
+  receipts_root     bytea not null,
+
+  gas_used          numeric not null,
+  gas_limit         numeric not null,
+  extra_data        bytea not null,
+  total_difficulty  numeric not null,
+  -- TODO: Account for `seal_fields`
+  size              numeric not null,
+  mix_hash          bytea,
+);
+
+create table ethereum_mainnet.transactions (
+  id                bytea primary key not null,
+
+  hash              bytea unique not null,
+  block             bytea not null references ethereum_mainnet.blocks(id),
+  transaction_index numeric not null,
+  nonce             numeric not null,
+
+  from              bytea not null,
+  to                bytea,
+  value             bytea not null,
+  input             bytea not null,
+
+  gas_price         numeric not null,
+  gas               numeric not null,
+)
+
+create table ethereum_mainnet.transaction_receipts (
+  id                  bytea primary key not null,
+
+  transaction         bytea unique not null references ethereum_mainnet.transactions(id),
+  contract            bytea not null references ethereum_mainnet.contracts(id),
+  logs_bloom          bytea not null,
+
+  cumulative_gas_used numeric not null,
+  gas_used            numeric not null,
+  status              int2 not null,
+)
+
+create table ethereum_mainnet.log (
+  id                    bytea primary key not null,
+
+  block                 bytea not null references ethereum_mainnet.blocks(id),
+  transaction           bytea not null references ethereum_mainnet.transactions(id),
+  log_index             numeric,
+  transaction_log_index numeric,
+  contract              bytea not null references ethereum_mainnet.contracts(id),
+
+  -- TODO: Account for `topics`
+
+  data                  bytea not null,
+  log_type              string,
+  removed               boolean not null,
+)
+
+create table ethereum_mainnet.accounts (
+  id      bytea primary key not null,
+  address bytea unique not null,
+  balance numeric not null,
+)
+
+create table ethereum_mainnet.contracts (
+  id      bytea primary key not null,
+  address bytea unique not null,
+  balance numeric not null,
+)
